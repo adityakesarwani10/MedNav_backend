a660fe03b15209087f1edddc1ab3beed44b4cca7
adityakesarwani10 <adityakesarwani109@gmail.com>
2026-04-25 23:57:28 +0530

    Update keep alive function

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const app = require("./app.js");
 const { connectDB } = require("./db.js");
+const keepAlive = require("./keepAlive");
+// server start hone ke baad
 
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 MedNav Server running cleanly on port ${PORT}`);
+  app.listen(3000, () => {
+    console.log("Server running");
+    keepAlive("https://mednav-backend.onrender.com/api/status");
   });
 });
+  // app.listen(PORT, () => {
+  //   console.log(`🚀 MedNav Server running cleanly on port ${PORT}`);
+  // });
 
