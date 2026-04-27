5bdc831f61991ce5c8075ca7eabf7b506d9c360d
adityakesarwani10 <adityakesarwani109@gmail.com>
2026-04-27 17:05:22 +0530

    Adding testing for web calling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors    = require("cors");
 const app     = express();
+const AccessToken       = require("twilio").jwt.AccessToken;
+const VoiceGrant        = AccessToken.VoiceGrant;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
@@ -12,7 +14,24 @@ app.use(express.static("public"));
 // Triage Agent routes (Twilio webhooks — return XML)
 // const voiceRoutes = require("./routes/voice");
 // app.use("/", voiceRoutes);
+// app.js mein add karo
 
+app.get("/api/token", (req, res) => {
+  const token = new AccessToken(
+    process.env.ACCOUNT_SID,
+    process.env.ANTHROPIC_API_KEY,        // Twilio Console se banao
+    process.env.API_SECRET,     // Twilio Console se banao
+    { identity: "demo-user" }
+  );
+
+  const voiceGrant = new VoiceGrant({
+    outgoingApplicationSid: process.env.TWIML_APP_SID, // Console se banao
+    incomingAllow: false,
+  });
+
+  token.addGrant(voiceGrant);
+  res.json({ token: token.toJwt() });
+});
 const router = require("./routes/router");
 app.use("/", router);
 
